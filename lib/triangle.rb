056380fe8674a95f4b1c05c3fe7ab5e0c97d5e43
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-06 20:16:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,17 @@ class Triangle
   end
 
   def kind
-
+    if a + b > c && a + c > b && c + b > a
+      if a == b && b == c
+        :equilateral
+      elsif a == b || a == c || b == c
+        :isosceles
+      else
+        :scalene
+      end
+    else
+        raise TriangleError
+    end
   end
 
   class TriangleError < StandardError
